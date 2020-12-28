f9f490dbd822a210960343eaf7d08fd3e053f431
Dmytro Znaiko <dznayko@gmail.com>
2020-12-28 17:57:27 +0100

    add solution for year '15, day 1

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,3 +16,12 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+dependencies {
+  testImplementation(platform("org.junit:junit-bom:5.7.0"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+  useJUnitPlatform()
+}
--- src/test/kotlin/year15/Year15Day1Test.kt
+++ src/test/kotlin/year15/Year15Day1Test.kt
@@ -9,49 +9,48 @@ class Year15Day1Test {
 
     val year15Day1 = Year15Day1()
 
-    @Nested
-    inner class TestPart1 {
+  @Nested
+  inner class TestPart1 {
 
-        @ParameterizedTest
-        @ValueSource(strings = ["(())", "()()"])
-        fun `should return 0`(s: String) {
-            assertEquals(0, year15Day1.part1(s))
-        }
-
-        @ParameterizedTest
-        @ValueSource(strings = ["(((", "(()(()(", "))((((("])
-        fun `should return 3`(s: String) {
-            assertEquals(3, year15Day1.part1(s))
-        }
-
-        @ParameterizedTest
-        @ValueSource(strings = ["())", "))("])
-        fun `should return -1`(s: String) {
-            assertEquals(-1, year15Day1.part1(s))
-        }
-
-        @ParameterizedTest
-        @ValueSource(strings = [")))", ")())())"])
-        fun `should return -3`(s: String) {
-            assertEquals(-3, year15Day1.part1(s))
-        }
-
+    @ParameterizedTest
+    @ValueSource(strings = ["(())", "()()"])
+    fun `should return 0`(s: String) {
+      assertEquals(0, year15Day1.part1(s))
     }
 
-    @Nested
-    inner class TestPart2 {
-
-        @ParameterizedTest
-        @ValueSource(strings = [")"])
-        fun `should return 1`(s: String) {
-            assertEquals(1, year15Day1.part2(s))
-        }
-
-        @ParameterizedTest
-        @ValueSource(strings = ["()())"])
-        fun `should return 5`(s: String) {
-            assertEquals(5, year15Day1.part2(s))
-        }
-
+    @ParameterizedTest
+    @ValueSource(strings = ["(((", "(()(()(", "))((((("])
+    fun `should return 3`(s: String) {
+      assertEquals(3, year15Day1.part1(s))
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["())", "))("])
+    fun `should return -1`(s: String) {
+      assertEquals(-1, year15Day1.part1(s))
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = [")))", ")())())"])
+    fun `should return -3`(s: String) {
+      assertEquals(-3, year15Day1.part1(s))
+    }
+
+  }
+
+  @Nested
+  inner class TestPart2 {
+
+    @ParameterizedTest
+    @ValueSource(strings = [")"])
+    fun `should return 1`(s: String) {
+      assertEquals(1, year15Day1.part2(s))
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["()())"])
+    fun `should return 5`(s: String) {
+      assertEquals(5, year15Day1.part2(s))
+    }
+  }
 }
