de076df809569ed40c5e32021189f94f667982c7
Dmytro Znaiko <dznayko@gmail.com>
2021-01-08 17:54:17 +0100

    add project version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,8 @@ repositories {
   mavenCentral()
 }
 
+version = "1.0"
+
 dependencies {
   testImplementation(platform("org.junit:junit-bom:5.7.0"))
   testImplementation("org.junit.jupiter:junit-jupiter")
