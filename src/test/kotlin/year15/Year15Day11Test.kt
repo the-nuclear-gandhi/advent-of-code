972e593f3bf320dff66e9a55f983e3fd62f0a73f
Dmytro Znaiko <dznayko@gmail.com>
2021-03-01 20:12:24 +0100

    add solution for year '15, day 11

---
+++ src/test/kotlin/year15/Year15Day11Test.kt
@@ -0,0 +1,35 @@
+package year15
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class Year15Day11Test {
+
+  val year15Day11 = Year15Day11()
+
+  @Nested
+  inner class TestPart1 {
+
+    @Test
+    fun `should return abcdffaa`() {
+      val input = "abcdefgh"
+      assertEquals("abcdffaa", year15Day11.part1(input))
+    }
+
+    @Test
+    fun `should return ghjaabcc`() {
+      val input = "ghijklmn"
+      assertEquals("ghjaabcc", year15Day11.part1(input))
+    }
+  }
+
+  @Nested
+  inner class TestPart2 {
+    @Test
+    fun `should return abcdffbb`() {
+      val input = "abcdefgh"
+      assertEquals("abcdffbb", year15Day11.part2(input))
+    }
+  }
+}
