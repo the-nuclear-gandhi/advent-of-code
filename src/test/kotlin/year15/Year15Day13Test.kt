1f247b224787340464bd7b20bd337679a28499c1
Dmytro Znaiko <dznayko@gmail.com>
2021-04-03 21:02:06 +0200

    add solution for year '15, day 13 using the algorithm for solving the traveling salesman problem implemented before

---
+++ src/test/kotlin/year15/Year15Day13Test.kt
@@ -0,0 +1,41 @@
+package year15
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class Year15Day13Test {
+
+  val year15Day13 = Year15Day13()
+  val input = listOf(
+    "Alice would gain 54 happiness units by sitting next to Bob.",
+    "Alice would lose 79 happiness units by sitting next to Carol.",
+    "Alice would lose 2 happiness units by sitting next to David.",
+    "Bob would gain 83 happiness units by sitting next to Alice.",
+    "Bob would lose 7 happiness units by sitting next to Carol.",
+    "Bob would lose 63 happiness units by sitting next to David.",
+    "Carol would lose 62 happiness units by sitting next to Alice.",
+    "Carol would gain 60 happiness units by sitting next to Bob.",
+    "Carol would gain 55 happiness units by sitting next to David.",
+    "David would gain 46 happiness units by sitting next to Alice.",
+    "David would lose 7 happiness units by sitting next to Bob.",
+    "David would gain 41 happiness units by sitting next to Carol.",
+  )
+
+  @Nested
+  inner class TestPart1 {
+
+    @Test
+    fun `should return 330`() {
+      Assertions.assertEquals(330, year15Day13.part1(input))
+    }
+  }
+
+  @Nested
+  inner class TestPart2 {
+    @Test
+    fun `should return 982`() {
+      Assertions.assertEquals(286, year15Day13.part2(input))
+    }
+  }
+}
