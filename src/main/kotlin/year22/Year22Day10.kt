4eb8882ef4cb98ebe556a305a014fbe66da38600
Dimitri Znaiko <dznayko@gmail.com>
2022-12-29 10:41:32 +0100

    add solution for year '22, day 10

---
+++ src/main/kotlin/year22/Year22Day10.kt
@@ -0,0 +1,56 @@
+package year22
+
+import shared.Day
+
+class Year22Day10 : Day<List<String>>() {
+    override fun getInput(): List<String> = inputResource().asLines()
+
+    override fun part1(input: List<String>): Int = with(Device()) {
+        val inputIterator = input.iterator()
+        while (this.cycles < 220) {
+            this.runCommand(inputIterator.next())
+        }
+
+        this.signalLevels.sum()
+    }
+
+    override fun part2(input: List<String>): String = with(Device()) {
+        input.forEach { this.runCommand(it) }
+            .let { this.crt }
+            .also { crt -> crt.chunked(40).forEach { println(it) } }
+    }
+
+    private class Device {
+        private var x = 1
+
+        var cycles = 0
+        var signalLevels = mutableListOf<Int>()
+        var crt = ""
+
+        fun runCommand(command: String) = when {
+            command.startsWith("noop") -> runCycle()
+            command.startsWith("addx") -> {
+                repeat(2) {
+                    runCycle()
+                }
+                x += command.substringAfter(" ").toInt()
+            }
+
+            else -> {}
+        }
+
+        private fun runCycle() {
+            crt += drawPixel(cycles, x)
+            cycles++
+            if (cycles % 40 == 20) {
+                signalLevels += x * cycles
+            }
+        }
+
+        private fun drawPixel(cycle: Int, x: Int): String = if (cycle % 40 in x - 1..x + 1) {
+            "#"
+        } else {
+            "."
+        }
+    }
+}
