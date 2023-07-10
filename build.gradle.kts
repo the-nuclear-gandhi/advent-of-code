ae4af21da4a76f70882b46d8b92f49394b86298b
Dimitri Znaiko <dznayko@gmail.com>
2023-07-10 09:30:17 +0200

    update Kotlin to 1.9.0, Gradle to 8.2, update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.8.20"
+    kotlin("jvm") version "1.9.0"
 }
 
 repositories {
@@ -9,8 +9,8 @@ repositories {
 version = "1.0"
 
 dependencies {
-    implementation("com.github.ajalt.clikt:clikt:3.5.4")
-    implementation("ch.qos.logback:logback-classic:1.3.7")
+    implementation("com.github.ajalt.clikt:clikt:4.0.0")
+    implementation("ch.qos.logback:logback-classic:1.4.8")
 
     testImplementation(platform("org.junit:junit-bom:5.9.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
