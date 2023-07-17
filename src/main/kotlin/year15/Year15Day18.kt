41279d9f34c94ce2fec8c2e955ef37316b0ca1e0
Dimitri Znaiko <dznayko@gmail.com>
2023-07-17 17:28:52 +0200

    add solution for year '15, day 18

---
+++ src/main/kotlin/year15/Year15Day18.kt
@@ -0,0 +1,57 @@
+package year15
+
+import shared.Day
+import shared.InputConverter.Companion.toLines
+import shared.Point
+
+class Year15Day18 : Day<List<String>>(::toLines) {
+    override fun part1(input: List<String>): Int = solvePart1(input, 100)
+
+    override fun part2(input: List<String>): Int = solvePart2(input, 100)
+
+    internal fun solvePart1(input: List<String>, times: Int): Int {
+        var lightMap = input.map { s ->
+            s.map { it == '#' }.toList()
+        }
+
+        repeat(times) {
+            lightMap = lightMap.mapIndexed { x, lightRow ->
+                lightRow.mapIndexed { y, b ->
+                    val neighboursOn = countNeighboursOn(lightMap, x, y)
+                    neighboursOn == 3 || (b && neighboursOn == 2)
+                }
+            }
+        }
+
+        return lightMap.flatten().count { it }
+    }
+
+    private fun countNeighboursOn(lightMap: List<List<Boolean>>, x: Int, y: Int): Int = Point(x, y)
+        .neighbours(true)
+        .filter { it.x in lightMap.indices && it.y in lightMap[0].indices }
+        .count { lightMap[it.x][it.y] }
+
+    internal fun solvePart2(input: List<String>, times: Int): Int {
+        val cornerPoints = setOf(
+            Point(0, 0),
+            Point(0, input[0].length - 1),
+            Point(input.size - 1, 0),
+            Point(input.size - 1, input[0].length - 1)
+        )
+
+        var lightMap = input.mapIndexed { x, s ->
+            s.mapIndexed { y, c -> Point(x, y) in cornerPoints || c == '#' }.toList()
+        }
+
+        repeat(times) {
+            lightMap = lightMap.mapIndexed { x, lightRow ->
+                lightRow.mapIndexed { y, b ->
+                    val neighboursOn = countNeighboursOn(lightMap, x, y)
+                    Point(x, y) in cornerPoints || (neighboursOn == 3 || (b && neighboursOn == 2))
+                }
+            }
+        }
+
+        return lightMap.flatten().count { it }
+    }
+}
---
+++ src/test/kotlin/year15/Year15Day18Test.kt
@@ -0,0 +1,39 @@
+package year15
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import shared.InputConverter
+
+class Year15Day18Test {
+
+    val year15Day18 = Year15Day18()
+    val input = InputConverter.toLines(
+        """
+            .#.#.#
+            ...##.
+            #....#
+            ..#...
+            #.#..#
+            ####..
+        """.trimIndent()
+    )
+
+    @Nested
+    inner class TestPart1 {
+
+        @Test
+        fun `should return 4`() {
+            assertEquals(4, year15Day18.solvePart1(input, 4))
+        }
+    }
+
+    @Nested
+    inner class TestPart2 {
+
+        @Test
+        fun `should return 17`() {
+            assertEquals(17, year15Day18.solvePart2(input, 5))
+        }
+    }
+}
