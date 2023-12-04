0aa0ddbb45ede5e5a695763a27b925785fae899b
Dimitri Znaiko <dznayko@gmail.com>
2023-12-04 17:36:10 +0100

    add solution for year '23, day 4

---
+++ src/main/kotlin/year23/Year23Day4.kt
@@ -0,0 +1,31 @@
+package year23
+
+import core.Day
+import core.InputConverter.Companion.toLines
+
+class Year23Day4 : Day<List<String>>(::toLines) {
+    override fun part1(input: List<String>): Int = parseInput(input)
+        .map { (winning, received) -> received.count { it in winning } }
+        .filter { it > 0 }
+        .sumOf { 1.shl(it - 1) }
+
+    override fun part2(input: List<String>): Int = IntArray(input.size) { 1 }
+        .apply {
+            parseInput(input).mapIndexed { index, (winning, received) ->
+                received.count { it in winning }
+                    .let { index + 1..index + it }
+                    .forEach { this[it] += this[index] }
+            }
+        }
+        .sum()
+
+    private fun parseInput(input: List<String>): List<Pair<List<Int>, List<Int>>> = input.map {
+        val winningNumbers = it.substringAfter(":").substringBefore("|").toIntList()
+        val receivedNumbers = it.substringAfter("|").toIntList()
+
+        winningNumbers to receivedNumbers
+    }
+
+    private fun String.toIntList(): List<Int> = this.trim().split(" ").mapNotNull { it.toIntOrNull() }
+}
+
