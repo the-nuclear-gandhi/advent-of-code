ba46616ed4f688e6f2463db2aa0987e1473d4d4d
Dimitri Znaiko <dznayko@gmail.com>
2024-04-10 18:41:58 +0200

    add solution for year '15, day 25

---
+++ src/main/kotlin/year15/Year15Day25.kt
@@ -0,0 +1,31 @@
+package year15
+
+import core.Day
+import core.InputConverter.Companion.noOp
+
+class Year15Day25 : Day<String>(::noOp) {
+    override fun part1(input: String): Long {
+        val targetRow = input.substringAfter("row").substringBefore(",").trim().toInt()
+        val targetColumn = input.substringAfter("column").substringBefore(".").trim().toInt()
+
+        var number = 20151125L
+        var row = 1
+        var rowLength = 2
+        var target = 0L
+        while (target == 0L) {
+            for (y in 0 until rowLength) {
+                number *= 252533
+                number %= 33554393
+                if (row - y == targetRow - 1 && y == targetColumn - 1) {
+                    target = number
+                }
+            }
+            row++
+            rowLength++
+        }
+
+        return target
+    }
+
+    override fun part2(input: String) = Unit
+}
---
+++ src/test/kotlin/year15/Year15Day25Test.kt
@@ -0,0 +1,11 @@
+package year15
+
+import core.DayTest
+
+class Year15Day25Test : DayTest<String, Long, Unit>(
+    Year15Day25::class.java,
+    listOf(
+        "row 6, column 6." to 27995004L,
+    ),
+    listOf()
+)
