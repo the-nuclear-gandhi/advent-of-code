69cb6024b60877db1a840501a0d47c807fdd7d14
Dimitri Znaiko <dznayko@gmail.com>
2024-07-07 14:17:36 +0200

    refactor solution for year '20, day 4

--- src/main/kotlin/year20/Year20Day4.kt
+++ src/main/kotlin/year20/Year20Day4.kt
@@ -19,22 +19,26 @@ class Year20Day4 : Day<List<LineBlock>>(::toLineBlocks) {
     private fun Map<String, String>.containsAllRequiredKeys(): Boolean = this.keys.containsAll(requiredKeys)
     private fun Map<String, String>.containsValidValues(): Boolean = this.entries.all { (key, value) ->
         when (key) {
-            "byr" -> value.isAFourDigitNumber() && value.toInt() in 1920..2002
-            "iyr" -> value.isAFourDigitNumber() && value.toInt() in 2010..2020
-            "eyr" -> value.isAFourDigitNumber() && value.toInt() in 2020..2030
+            "byr" -> value.isNumberOfLengthAndInRange(4, 1920..2002)
+            "iyr" -> value.isNumberOfLengthAndInRange(4, 2010..2020)
+            "eyr" -> value.isNumberOfLengthAndInRange(4, 2020..2030)
             "hgt" -> if (value.contains("cm")) {
-                value.takeWhile { c -> c.isDigit() }.toInt() in 150..193
+                value.takeWhile { it.isDigit() }.toInt() in 150..193
             } else {
-                value.takeWhile { c -> c.isDigit() }.toInt() in 59..76
+                value.takeWhile { it.isDigit() }.toInt() in 59..76
             }
+
             "hcl" -> value.matches(Regex("#[0-9a-f]{6}"))
-            "ecl" -> value in setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
-            "pid" -> value.matches(Regex("\\d{9}"))
+            "ecl" -> value in validEyeColor
+            "pid" -> value.isNumberOfLength(9)
             else -> true
         }
     }
 
-    private fun String.isAFourDigitNumber(): Boolean = this.matches(Regex("\\d{4}"))
+    private fun String.isNumberOfLength(length: Int): Boolean = this.length == length && this.all { it.isDigit() }
+    private fun String.isNumberOfLengthAndInRange(length: Int, range: IntRange): Boolean =
+        this.isNumberOfLength(length) && this.toInt() in range
 
     private val requiredKeys = setOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+    private val validEyeColor = setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
 }
