0f83f546200527b622300ea72cbb887db4f95ddf
Dimitri Znaiko <dznayko@gmail.com>
2025-12-17 10:04:29 +0100

    add solution for year '25, day 7

--- src/main/kotlin/year25/Year25Day7.kt
+++ src/main/kotlin/year25/Year25Day7.kt
@@ -5,50 +5,35 @@ import core.InputConverter.Companion.toLines
 
 class Year25Day7 : Day<List<String>>(::toLines) {
 
-    override fun part1(input: List<String>): Int = parseInput(input).let {
-        var activeSplitters = 0
+    override fun part1(input: List<String>): Int = calculateLightPaths(input).second
+
+    override fun part2(input: List<String>): Long = calculateLightPaths(input).first.last().sum()
+
+    private fun calculateLightPaths(input: List<String>): Pair<Array<LongArray>, Int> = parseInput(input).let {
+        var activations = 0
 
         (1..it.lastIndex).forEach { i ->
-            it[i].indices.filter { j -> it[i - 1][j] != 0L }.forEach { j ->
-                when (input[i][j]) {
-                    '.' -> it[i][j] = it[i - 1][j]
-                    '^' -> {
-                        if (j > 0) {
-                            it[i][j - 1] = it[i - 1][j]
-                        }
-
-                        if (j < it[i].size - 1) {
-                            it[i][j + 1] = it[i - 1][j]
-                        }
-
-                        activeSplitters++
-                    }
-                }
-            }
-        }
-
-        return activeSplitters
-    }
-
-    override fun part2(input: List<String>): Long = parseInput(input).apply {
-        (1..this.lastIndex).forEach { i ->
-            this[i].indices.filter { this[i - 1][it] != 0L }
+            it[i].indices.filter { j -> it[i - 1][j] != 0L }
                 .forEach { j ->
                     when (input[i][j]) {
-                        '.' -> this[i][j] += this[i - 1][j]
+                        '.' -> it[i][j] += it[i - 1][j]
                         '^' -> {
                             if (j > 0) {
-                                this[i][j - 1] += this[i - 1][j]
+                                it[i][j - 1] += it[i - 1][j]
                             }
 
-                            if (j < this[i].size - 1) {
-                                this[i][j + 1] += this[i - 1][j]
+                            if (j < it[i].size - 1) {
+                                it[i][j + 1] += it[i - 1][j]
                             }
+
+                            activations++
                         }
                     }
                 }
         }
-    }.last().sum()
+
+        it to activations
+    }
 
     private fun parseInput(input: List<String>): Array<LongArray> =
         Array(input.size) { LongArray(input[0].length) { 0L } }.apply {
